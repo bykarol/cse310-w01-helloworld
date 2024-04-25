80a3e4d01b304565137b87e025049d0442993c07
Karol Bracho <brachokarol.sud@gmail.com>
2024-04-25 20:02:39 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+      <div className="container-l min-vh-100 bg-light">
+        <div className='p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end'>
+          <h1 className="text-center">Hello World from React!!</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
