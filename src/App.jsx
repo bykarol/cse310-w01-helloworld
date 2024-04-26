9ab38dfa1a771b183d4f2cecb39e411e511d1a62
Karol Bracho <brachokarol.sud@gmail.com>
2024-04-26 12:01:05 +0200

    changing the displayed msg

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
     <>
       <div className="container-l min-vh-100 bg-light">
         <div className='p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end'>
-          <h1 className="text-center">Hello World from React!!</h1>
+          <h1 className="text-center">Hello World from React and GitHub Pages!!</h1>
         </div>
       </div>
     </>
